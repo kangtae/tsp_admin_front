9a60aa2b1782a2b475019b4ffeafb478ba668568
kangtaewan <companytaewan@gmail.com>
2021-09-14 13:25:10 +0900

    git test

--- src/components/common/ContentHeader.vue
+++ src/components/common/ContentHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="cont_tit">
     <h2>남자모델</h2>
-    <div class="location"><span>홈</span> &gt; 모델 관리 &gt; 남자모델</div>
+    <div class="location">
+      <span>홈</span> &gt; 모델 관리 &gt; 남자모델 test
+    </div>
   </div>
 </template>
 
